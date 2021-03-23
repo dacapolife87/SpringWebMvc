2ea548af6e36268001438859f1c9060cc3e61fa3
HyungjuJang <dacapolife87@gmail.com>
2021-03-23 09:38:12 +0900

    Initial commit

---
+++ src/main/java/me/hjjang/webmvc/WebmvcApplication.java
@@ -0,0 +1,13 @@
+package me.hjjang.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebmvcApplication.class, args);
+    }
+
+}
