2ffac1e377c9252c5985dfadc576e7b90b3a3e86
HyungjuJang <dacapolife87@gmail.com>
2021-03-24 17:12:37 +0900

    feat : HelloServlet 예제 및 HttpServletRequest 샘플 예제

--- src/main/java/me/hjjang/webmvc/WebmvcApplication.java
+++ src/main/java/me/hjjang/webmvc/WebmvcApplication.java
@@ -2,7 +2,9 @@ package me.hjjang.webmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동등록
 @SpringBootApplication
 public class WebmvcApplication {
 
