f74bd1255302d75d1fcb05c04fbeb11bc83caaac
HyungjuJang <dacapolife87@gmail.com>
2021-03-27 09:58:10 +0900

    feat : HttpRequest Json 용 dto 생성

---
+++ src/main/java/me/hjjang/webmvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package me.hjjang.webmvc.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+}
