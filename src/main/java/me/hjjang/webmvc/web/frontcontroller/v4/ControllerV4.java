1a5c5d2a21563d6bf5ae9536b26de2cc425ce4c4
HyungjuJang <dacapolife87@gmail.com>
2021-04-11 14:08:07 +0900

    feat : FrontController V4 - V4버전 컨트롤러 인터페이스 작성 - Param및 Model 적용

---
+++ src/main/java/me/hjjang/webmvc/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,13 @@
+package me.hjjang.webmvc.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     * @param paramMap
+     * @param model
+     * @return
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+}
